c482c5493f993d5c27ffe78dd524a3e3b2037e95
uwandera <tomuwande@gmail.com>
2024-05-02 21:52:49 -0300

    adicição de validação!

--- cadastro.py
+++ cadastro.py
@@ -7,19 +7,37 @@ print("==============================")
 print("#       Cadastramento        #")
 print("="*30)
 
-vcpf = input("digite apenas os numeros do CPF: ")
-vnome = input("digite seu NOME: ")
-vgenero = input("Qual seu genero: ")
-vsexo = input("Qual seu sexo biologico: ")
-vorientacao = input("qual a sua orientação sexual: ")
-vusuario = input("qual nome de Usuario: ")
-vsenha = input("Password: ")
-vconfirma = input("deseja confirmar os dados:(S ou N) ")
-if vconfirma.upper() == "S":
+while True: #controle de execução
+
+    while True: #verificação do cpf  
+        vcpf = input("digite apenas os numeros do CPF: ")
+        if vcpf.isdigit():#
+            break
+        else:
+            print("digite um valor numerico")
+    while True: 
+        vnome = input("digite seu NOME: ")
+        if vnome.isalpha():
+            break
+        elif vnome == "":
+            print("nome é uma variavel não nula")
+            break
+
+            
+    vgenero = input("Qual seu genero: ")
+    vsexo = input("Qual seu sexo biologico: ")
+    vorientacao = input("qual a sua orientação sexual: ")
+    vusuario = input("qual nome de Usuario: ")
+    vsenha = input("Password: ")
+    vconfirma = input("deseja confirmar os dados:(S ou N) ")
+    if vconfirma.upper() == "S":
     # enviar instrução sql para ser executada pelo banco
-    conn.execute("insert into cadastro values(?,?,?,?,?,?,?)", (vcpf,vnome,vgenero,vsexo,vorientacao,vusuario,vsenha))
-    conn.commit()
-    print("cadastro feito com sucesso!")   
+        conn.execute("insert into cadastro values(?,?,?,?,?,?,?)", (vcpf,vnome,vgenero,vsexo,vorientacao,vusuario,vsenha))
+        conn.commit()
+        print("cadastro feito com sucesso!")
+      
+
+## tem que fazer o crud:  update, delete, select
 ## fechar conexão 
 conn.close
 quit()
